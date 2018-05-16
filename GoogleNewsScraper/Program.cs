77be4382d0ee73747a63505dea75323d2e32d5c6
Alexander Kuznetsov <sasha.kuznetsov@queensu.ca>
2018-05-16 12:51:53 -0400

    Initial Commit

---
+++ GoogleNewsScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoogleNewsScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
